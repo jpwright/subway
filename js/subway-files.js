693aa62a8bd1ad9ebb9b3d45db81ecb1ba53f70a
bootmii <alt.people.davidcalman@gmail.com>
2023-11-07 15:41:20 -0800

    Implement jpwright/subway#21

--- js/subway-files.js
+++ js/subway-files.js
@@ -148,5 +148,5 @@ function save_game_json() {
         $(this).remove()
     })[0].click();*/
     var blob = new Blob([JSON.stringify(json)], {type: "text/plain;charset=utf-8"});
-    saveAs(blob, "bns_saved_game.json");
+    saveAs(blob, "bns_saved_game.json", { autoBom: false });
 }
